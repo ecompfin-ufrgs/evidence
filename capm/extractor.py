c6d54197bbbcb54df27998c741a7f657fb675609
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2024-09-18 15:38:58 -0300

    Update extractor.py

--- capm/extractor.py
+++ capm/extractor.py
@@ -5,5 +5,7 @@ import pandas as pd
 
 
 def get_stock_price_b3(codigo: str, data: list) -> list:
+  """Esta função coleta dados de ações da B3.  Seus argumentos
+  são o código da ação e uma lista com datas desejadas"""
   pass
   
