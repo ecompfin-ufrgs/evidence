21d72278d50c7b49ed8d76164aa914fc170af44f
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2024-09-18 15:58:24 -0300

    Update extractor.py

--- capm/extractor.py
+++ capm/extractor.py
@@ -5,7 +5,12 @@ import pandas as pd
 
 
 def get_stock_price_b3(codigo: str, data: list) -> list:
-  """Esta função coleta dados de ações da B3.  Seus argumentos
+  """Esta função coleta dados de ações da B3 no Yahoo Finance.  Seus argumentos
   são o código da ação e uma lista com datas desejadas"""
   pass
-  
+
+
+def get_cdi(data: list) -> list:
+  """Esta função coleta dados de ações do CDI no Yahoo Finance.  Seus argumentos
+  são o código da ação e uma lista com datas desejadas"""
+  pass
