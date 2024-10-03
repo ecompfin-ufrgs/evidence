789653cfddc41eb04c6821d13e2e424964b374a0
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2024-10-03 16:28:15 -0300

    Add files via upload

---
+++ capm/v001/app/modulo1.py
@@ -0,0 +1,10 @@
+"""
+
+aqui vai a documentação
+
+
+"""
+
+def add(x: int, y: int) -> int:
+    """Esta função soma dois números inteiros."""
+    return x + y
---
+++ capm/v001/docs/capm.md
@@ -0,0 +1,3 @@
+# Modelo CAPM
+
+Este modelo foi criado em [Sharpe (1964)](https://onlinelibrary.wiley.com/doi/10.1111/j.1540-6261.1964.tb02865.x)
