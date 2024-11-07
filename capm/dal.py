80f0ef9e4e3c26f866f1da75338d79ea11bf7aa4
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2024-11-07 14:48:05 -0300

    Create dal.py

---
+++ capm/dal.py
@@ -0,0 +1,22 @@
+"""
+Descrição: Este módulo implementa classes e funções para manipulação de banco de dados
+Autor: Vitor e Murilo
+Data: 07/11/2024
+Versão: 0.0.1
+"""
+
+
+def create():
+  pass
+
+def update():
+  pass
+
+def retrive():
+  pass
+
+def delete():
+  pass
+
+
+
